cb678f51f651665b7b6852def62b994fda361f63
Michael Newell <michael.newell212@gmail.com>
2023-07-28 16:05:44 -0600

    wip

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -24,7 +24,7 @@ async function checkUsernameFree(req, res, next) {
   try {
     const users = await User.findBy({ username: req.body.username })
     if(!users.length) next()
-    else next({ message: "Username taken"})
+    else next({ message: "Username taken", status: 422})
   } catch(err) {
     next(err)
   }
@@ -38,8 +38,14 @@ async function checkUsernameFree(req, res, next) {
     "message": "Invalid credentials"
   }
 */
-function checkUsernameExists(req, res, next) {
-  next()
+async function checkUsernameExists(req, res, next) {
+  try {
+    const users = await User.findBy({ username: req.body.username })
+    if(users.length) next()
+    else next({ message: "Invalid credentials", status: 401})
+  } catch(err) {
+    next(err)
+  }
 }
 
 /*
